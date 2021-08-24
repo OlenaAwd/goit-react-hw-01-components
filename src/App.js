cb1a8090077972db2c5db9cf85264353ed064a47
OlenaAwd <elena.avdiunina@gmail.com>
2021-08-24 23:56:58 +0300

    start hw1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Profile from './components/social-profile/Profile';
+import user from './components/social-profile/user.json';
+import Statistics from './components/statistics/Statistics';
+import statisticalData from './components/statistics/statistical-data.json';
+import FriendList from './components/FriendList/FriendList';
+import friends from './components/FriendList/friends.json';
+import TransactionHistory from './components/transactions/TransactionHistory';
+import transactions from './components/transactions/transactions.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
